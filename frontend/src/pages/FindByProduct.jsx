338ece9135894d9840289e471a3f61b22b58d0e1
Claravasqz <claravasquez94@gmail.com>
2024-10-09 12:52:16 -0400

    This is the page of Find by Product

---
+++ frontend/src/pages/FindByProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FindByProduct() {
+    return(
+        <div>
+            <h1>Find Products by Product Name</h1>
+            <p>This is where the list of products will be displayed</p>
+        </div>
+    );
+}
+
+export default FindByProduct;
