ca01b365f1fb964c31779a8c991bad262cad3576
Claravasqz <claravasquez94@gmail.com>
2024-10-09 15:52:29 -0400

    button for products

--- frontend/src/pages/FindByStore.jsx
+++ frontend/src/pages/FindByStore.jsx
@@ -2,28 +2,47 @@ import React, {useEffect, useState } from 'react';
 
 function FindByStore() {
     const [stores, setStores] = useState([]);
+    const [selectedStore, setSelectedStore] = useState(null);
 
     useEffect(() => {
         const fetchStores = async() =>{
             const response = await fetch('http://localhost:3000/stores');
             const data = await response.json();
             setStores(data);
-
         };
 
         fetchStores();
     }, []);
 
+    const viewProducts = async (storeId) => {
+        const response = await fetch(`http://localhost:3000/stores/${storeId}`);
+        const data = await response.json();
+        setSelectedStore(data);
+    };
 
     return(
         <div>
             <h1>Find Products by Store</h1>
             <ul>
-                {stores.map((store) => (<lil key={store._id}>   
+                {stores.map((store) => (<li key={store._id}>   
                     {store.name} - {store.address}
-                </lil>
+                    <button onClick={() => viewProducts(store._id)}>View Products</button>
+                </li>
             ))}
             </ul>
+
+            {selectedStore && (
+                <div>
+                    <h2>Products in {selectedStore.name}</h2>
+                    <ul>
+                        {selectedStore.products.map((product) => (
+                            <li key={product._id}>
+                                {product.name} - ${product.price}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     );
 }
