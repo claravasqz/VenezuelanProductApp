3b912049a6ed8f7800473d946f2c302b9ebbbfb6
Claravasqz <claravasquez94@gmail.com>
2024-10-09 15:04:30 -0400

    Fetching Products

--- frontend/src/pages/FindByProduct.jsx
+++ frontend/src/pages/FindByProduct.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function FindByProduct() {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await fetch('http://localhost:3000/products');
+            const data = await response.json();
+            setProducts(data);
+        };
+
+    fetchProducts();
+    }, []);
+
     return(
         <div>
             <h1>Find Products by Product Name</h1>
-            <p>This is where the list of products will be displayed</p>
+            <ul>
+                {products.map((product) => (
+                    <li key={product._id}>
+                        {product.name} - ${product.price} ({product.category})
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
