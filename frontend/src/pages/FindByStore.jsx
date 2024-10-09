d0035f4dbacc1302410ac39e59a3f29a1e92a215
Claravasqz <claravasquez94@gmail.com>
2024-10-09 12:49:06 -0400

    Find by Products Page

---
+++ frontend/src/pages/FindByStore.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FindByStore() {
+    return(
+        <div>
+            <h1>Find Products by Store</h1>
+            <p>This is where the list of stores will be displayed.</p>
+        </div>
+    );
+}
+
+export default FindByStore;
