e2b29bc30276ce60e7637a8882691f7906e1ddd9
Claravasqz <claravasquez94@gmail.com>
2024-10-09 12:54:39 -0400

    MostPopularProducts Page

---
+++ frontend/src/pages/MostPopularProducts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MostPopularProducts() {
+    return(
+        <div>
+            <h1>Most Popular Products</h1>
+            <p>This is where the most popular products will be displayed</p>
+        </div>
+    );
+}
+
+export default MostPopularProducts;
