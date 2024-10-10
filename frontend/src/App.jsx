c7ed7b2ce0864cdfd5fdbf8226410cf5b2a20875
Claravasqz <claravasquez94@gmail.com>
2024-10-10 02:15:57 -0400

    Fixing Style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,21 +13,23 @@ function App() {
 
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li> <Link to="/">Home</Link></li>
-            <li> <Link to="/stores">Find By Store</Link></li>
-            <li><Link to="/products">Find by Product</Link></li>
-            <li><Link to="/popular-products">Most Popular Products</Link></li>
+      <div className="appContainer">
+        <nav className="navbar">
+          <ul className="navbarList">
+            <li className='navbarItem'> <Link className="navbarLink" to="/">Home</Link></li>
+            <li className='navbarItem'> <Link className="navbarLink" to="/stores">Find By Store</Link></li>
+            <li className='navbarItem'><Link className="navbarLink" to="/products">Find by Product</Link></li>
+            <li className='navbarItem'><Link className="navbarLink" to="/popular-products">Most Popular Products</Link></li>
           </ul>
         </nav>
+        <div className="content">
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/stores" element={<FindByStore />}/>
           <Route path="/products" element={<FindByProduct />}/>
           <Route path="/popular-products" element={<MostPopularProducts />}/>
         </Routes>
+        </div>
       </div>
     </Router>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 function Home(){
     return(
-        <div>
-            <h1>Venezuelan Product Finder</h1>
-            <p>Welcome! Choose how you want to search for products:</p>
-            <button onClick={() => window.location.href= "/stores"}>Find by Store</button>
-            <button onClick={() => window.location.href = "/products"}>Find by Product</button>
-            <button onClick={() => window.location.href = "/popular-products"}>Most Popular Products</button>
+        <div className="homeContainer">
+            <h1 className="homeTitle">Venezuelan Product Finder</h1>
+            <p className="homeDescription">Welcome! Choose how you want to search for products:</p>
+            <div className="buttonGroup">
+            <button className="homeButton" onClick={() => window.location.href= "/stores"}>Find by Store</button>
+            <button className="homeButton" onClick={() => window.location.href = "/products"}>Find by Product</button>
+            <button className="homeButton" onClick={() => window.location.href = "/popular-products"}>Most Popular Products</button>
+            </div>
         </div>
     );
 }
